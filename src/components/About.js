0422f0051890d6f151a526f97672c1ff9d982f5c
Olalekan Onaolapo <70687294+B0bbycode@users.noreply.github.com>
2024-01-01 18:16:36 +0200

    git commit changes

--- src/components/About.js
+++ src/components/About.js
@@ -19,27 +19,31 @@ class About extends React.Component {
   render() {
     const one = (
       <p>
-        I am currently a <b>Software Development Engineer</b> at
-        <a href="https://www.aboutamazon.com/"> Amazon</a>, working in the AWS
-        sector under team Route 53. At the same time, I am undertaking a
-        part-time <b> Master's of Science</b> in <b>Software Engineering </b>at{" "}
-        <a href="https://www.ox.ac.uk/about">University of Oxford</a>.
+        I currently serve as a <b>Software Development Engineer</b> at <a href="https://www.linconwavesinnovation.com/">Linconwaves Innovation</a>, 
+        where my role revolves around crafting the next generation of software solutions. I contribute to 
+        innovative projects that push the boundaries of technology, working collaboratively with a dynamic 
+        team to create impactful software experiences.
       </p>
     );
     const two = (
       <p>
-        Outside of work, I'm interested in following the developments of
-        science. I also play a lot of video games. And make TikToks.
+        Outside of the professional arena, my curiosity extends to the latest breakthroughs in
+        science. I am avidly engaged in keeping pace with scientific developments, attending 
+        conferences, and exploring how technology intersects with various fields. When not immersed
+        in code, I find joy in the immersive realms of video games and board games, allowing me to
+        unwind and tap into my creative side.
       </p>
     );
 
     const tech_stack = [
       "Typescript",
-      "Python",
+      "Node.js",
       "React.js",
-      "Java",
+      "Next.js",
+      "Mongoose",
+      "ScyllaDB",
       "Javascript ES6+",
-      "C#"
+      "C++"
     ];
 
     return (
@@ -51,7 +55,7 @@ class About extends React.Component {
           <div className="about-content">
             <div className="about-description">
               {[one]}
-              {"Here are some technologies I have been working with:"}
+              {"Techstacks I work with:"}
               <ul className="tech-stack">
                 {tech_stack.map(function (tech_item, i) {
                   return (
@@ -64,7 +68,7 @@ class About extends React.Component {
               {[two]}
             </div>
             <div className="about-image">
-              <img alt="Gazi Jarin" src={"/assets/me2.jpg"} />
+              <img alt="Chris Grey" src={"/assets/me.jpg"} />
             </div>
           </div>
         </FadeInSection>
